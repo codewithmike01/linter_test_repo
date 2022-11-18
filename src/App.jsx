47b465cdfb419f7b69e397b3bb86802adf5ac459
ginohmk <michotall95@gmail.com>
2022-11-18 02:18:16 +0100

    feat: Add external pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import Footer from "./components/footer/Footer";
 import Nav from "./components/nav/Nav";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing_page/LandingPage";
+import AboutUs from "./pages/about_us/AboutUs";
+import FAQs from "./pages/faqs/FAQs";
+import ContactUs from "./pages/contact_us/ContactUs";
+import TermsAndCondition from "./pages/terms_condition/TermsAndCondition";
+import Privacy from "./pages/privacy/Privacy";
+import Cookies from "./pages/cookies/Cookies";
+import Career from "./pages/career/Career";
+import Blog from "./pages/blog/Blog";
 
 function App() {
     return (
         <>
             <Nav />
+
+            <Routes>
+                <Route exact path="/" element={<LandingPage />} />
+                <Route exact path="/about" element={<AboutUs />} />
+                <Route exact path="/faqs" element={<FAQs />} />
+                <Route exact path="/contact" element={<ContactUs />} />
+                <Route exact path="/t&c" element={<TermsAndCondition />} />
+                <Route exact path="/privacy" element={<Privacy />} />
+                <Route exact path="/cookies" element={<Cookies />} />
+                <Route exact path="/career" element={<Career />} />
+                <Route exact path="/blog:id" element={<Blog />} />
+            </Routes>
             <Footer />
         </>
     );
